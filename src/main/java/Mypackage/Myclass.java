f348bae0c1171aa6f2d45474d1ad5566a9583eab
avnish <kkumar.avnishh@gmail.com>
2022-11-05 18:33:34 +0530

    first commit

---
+++ src/main/java/Mypackage/Myclass.java
@@ -0,0 +1,28 @@
+package Mypackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class Myclass {
+	
+		public void methods() {
+	   // System.setProperty("webdriver.chrome.driver","E:\\Avnish\\auto\\Coding\\Eclipse_workspace\\chromedriver\\chromedriver.exe");
+			
+			ChromeDriverManager.getInstance().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://google.com");	
+			String url = driver.getCurrentUrl();
+			System.out.println(url);
+
+			
+			
+			
+
+		}
+
+	}
+
+
+
---
+++ src/test/java/Mypackagetest/myclasstest.java
@@ -0,0 +1,21 @@
+package Mypackagetest;
+
+import org.testng.annotations.Test;
+
+import Mypackage.Myclass;
+
+public class myclasstest {
+	
+	@Test
+	public void methodtest() {
+		
+		Myclass my = new Myclass();
+		my.methods();
+		
+		
+		
+	}
+	
+	
+
+}
